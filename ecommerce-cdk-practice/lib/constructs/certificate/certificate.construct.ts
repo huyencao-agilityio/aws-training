2e96c202fde6867ef226f8aabbc6bca5571ff5c6
Huyen Cao <huyen.cao@asnet.com.vn>
2025-05-09 16:16:42 +0700

    feat(cert): implement stack and construct to request certificate for domain

---
+++ ecommerce-cdk-practice/lib/constructs/certificate/certificate.construct.ts
@@ -0,0 +1,32 @@
+import { Construct } from 'constructs';
+import {
+  Certificate,
+  CertificateValidation,
+  ICertificate
+} from 'aws-cdk-lib/aws-certificatemanager';
+import { RemovalPolicy } from 'aws-cdk-lib';
+
+import { DOMAIN_NAME } from '@constants/domain.constant';
+import { CertificateConstructProps } from '@interfaces/construct.interface';
+
+/**
+ * Define the construct to create certificate for domain
+ */
+export class CertificateConstruct extends Construct {
+  public readonly certificate: ICertificate;
+
+  constructor(scope: Construct, id: string, props: CertificateConstructProps) {
+    super(scope, id);
+
+    const { hostedZone } = props;
+
+    // Create new certificate for domain
+    this.certificate = new Certificate(this, 'WildcardCertificate', {
+      domainName: `*.${DOMAIN_NAME}`,
+      validation: CertificateValidation.fromDns(hostedZone),
+    });
+
+    // Avoid remove certificate when destroy stack
+    this.certificate.applyRemovalPolicy(RemovalPolicy.RETAIN);
+  }
+}
---
+++ ecommerce-cdk-practice/lib/stacks/certificate.stack.ts
@@ -0,0 +1,22 @@
+import { Stack } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+import { CertificateStackProps } from '@interfaces/stack.interface';
+
+import {
+  CertificateConstruct
+} from '../constructs/certificate/certificate.construct';
+
+export class CertificateStack extends Stack {
+  public readonly certificateConstruct: CertificateConstruct;
+
+  constructor(scope: Construct, id: string, props: CertificateStackProps) {
+    super(scope, id, props);
+
+    const { hostedZone } = props;
+
+    this.certificateConstruct = new CertificateConstruct(this, 'CertificateConstruct', {
+      hostedZone
+    });
+  }
+}
