52477bedec6677d40a2980f92fe94cf18e98f0df
Huyen Cao <huyen.cao@asnet.com.vn>
2025-05-12 15:42:28 +0700

    feat(refactor): refactor vpc class construct

--- ecommerce-cdk-practice/lib/constructs/vpc/vpc.construct.ts
+++ ecommerce-cdk-practice/lib/constructs/vpc/vpc.construct.ts
@@ -1,5 +1,12 @@
 import { Construct } from 'constructs';
-import { Vpc, SubnetType, IpAddresses, Peer, Port, SecurityGroup } from 'aws-cdk-lib/aws-ec2';
+import {
+  Vpc,
+  SubnetType,
+  IpAddresses,
+  Peer,
+  Port,
+  SecurityGroup
+} from 'aws-cdk-lib/aws-ec2';
 
 /**
  * Define the construct to create a new VPC
@@ -11,7 +18,15 @@ export class VpcConstruct extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    this.vpc = new Vpc(this, 'CdkAppVpc', {
+    this.vpc = this.createVpc();
+    this.securityGroup = this.createSecurityGroup();
+  }
+
+  /**
+   * Create a new VPC
+   */
+  createVpc(): Vpc {
+    const vpc = new Vpc(this, 'CdkAppVpc', {
       vpcName: 'CdkAppVpc',
       ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
       maxAzs: 2,
@@ -28,26 +43,33 @@ export class VpcConstruct extends Construct {
         },
       ],
       enableDnsSupport: true,
-      enableDnsHostnames: true
+      enableDnsHostnames: true,
     });
 
-    this.securityGroup = new SecurityGroup(this, 'SecurityGroupCdk', {
+    return vpc;
+  }
+
+  /**
+   * Create security group for VPC
+   * @returns
+   */
+  createSecurityGroup(): SecurityGroup {
+    const securityGroup = new SecurityGroup(this, 'SecurityGroup', {
       vpc: this.vpc,
       allowAllOutbound: true,
     });
 
-    // Add IngressRule for Security Group
-    this.securityGroup.addIngressRule(
+    securityGroup.addIngressRule(
       Peer.anyIpv4(),
       Port.allTcp(),
-      'Allow all TCP traffic from anywhere'
+      'Allow all TCP traffic from anywhere');
+
+    securityGroup.addIngressRule(
+      securityGroup,
+      Port.allTraffic(),
+      'Allow all traffic from self'
     );
 
-    // Allow traffic from Security Group of RDS
-    this.securityGroup.addIngressRule(
-      this.securityGroup,
-      Port.allTraffic(),
-      'Allow all traffic from RDS Security Group'
-    );
+    return securityGroup;
   }
 }
