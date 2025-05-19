faa9d1ebd295b4c50a7a4b52044a1ef7d2a88fb4
Huyen Cao <huyen.cao@asnet.com.vn>
2025-05-19 16:37:56 +0700

    feat(domain): update domain name for API

--- ecommerce-cdk-practice/src/constants/domain.constant.ts
+++ ecommerce-cdk-practice/src/constants/domain.constant.ts
@@ -4,12 +4,12 @@ export const BASE_URL = `https://${DOMAIN_NAME}`;
 export const ENVIRONMENTS = {
   staging: {
     apiGateway: {
-      domainName: 'staging-api.ecommerce-app.click',
+      domainName: 'api-staging.ecommerce-app.click',
       basePathApi: 'v1',
       stage: 'v1',
     },
     cloudFront: {
-      domainName: 'cdn.ecommerce-app.click',
+      domainName: 'cdn-staging.ecommerce-app.click',
     },
     cognito: {
       domainName: 'auth-staging.ecommerce-app.click',
@@ -17,15 +17,15 @@ export const ENVIRONMENTS = {
   },
   testing: {
     apiGateway: {
-      domainName: 'api.ecommerce-app.click',
+      domainName: 'api-testing.ecommerce-app.click',
       basePathApi: 'v1',
       stage: 'v1',
     },
     cloudFront: {
-      domainName: 'cdn.ecommerce-app.click',
+      domainName: 'cdn-testing.ecommerce-app.click',
     },
     cognito: {
-      domainName: 'auth-staging.ecommerce-app.click',
+      domainName: 'auth-testing.ecommerce-app.click',
     },
   },
   // Add more environments here as needed
