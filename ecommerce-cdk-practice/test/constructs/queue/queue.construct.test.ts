d77e98a931c35f8257855e566c4f8075cb5e2308
Huyen Cao <huyen.cao@asnet.com.vn>
2025-05-23 17:31:56 +0700

    feat(test): implement unit test for queue construct

---
+++ ecommerce-cdk-practice/test/constructs/queue/queue.construct.test.ts
@@ -0,0 +1,83 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+
+import { QueueConstruct } from '@constructs/queue/queue.construct';
+
+describe('QueueConstruct', () => {
+  let app: App;
+  let stack: Stack;
+  let template: Template;
+
+  beforeEach(() => {
+    app = new App();
+    stack = new Stack(app, 'TestQueueStack');
+  });
+
+  describe('Standard Queue', () => {
+    beforeEach(() => {
+      new QueueConstruct(stack, 'TestQueueConstruct', {
+        baseName: 'test-queue'
+      });
+
+      template = Template.fromStack(stack);
+    });
+
+    it('should create main queue and DLQ Queue', () => {
+      template.resourceCountIs('AWS::SQS::Queue', 2);
+    });
+
+    it('should create main queue with correct configuration', () => {
+      template.hasResourceProperties('AWS::SQS::Queue', {
+        QueueName: 'test-queue',
+        VisibilityTimeout: 30,
+        RedrivePolicy: {
+          maxReceiveCount: 2,
+          deadLetterTargetArn: Match.anyValue()
+        }
+      });
+    });
+
+    it('should create DLQ with correct configuration', () => {
+      template.hasResourceProperties('AWS::SQS::Queue', {
+        QueueName: 'test-queuedlq',
+        MessageRetentionPeriod: 345600
+      });
+    });
+  });
+
+  describe('FIFO Queue', () => {
+    beforeEach(() => {
+      new QueueConstruct(stack, 'TestFifoQueueConstruct', {
+        baseName: 'test-fifo-queue',
+        isFifo: true,
+        maxReceiveCount: 3
+      });
+
+      template = Template.fromStack(stack);
+    });
+
+    it('should create FIFO main queue and DLQ', () => {
+      template.resourceCountIs('AWS::SQS::Queue', 2);
+    });
+
+    it('should create FIFO main queue with correct configuration', () => {
+      template.hasResourceProperties('AWS::SQS::Queue', {
+        QueueName: 'test-fifo-queue.fifo',
+        VisibilityTimeout: 30,
+        FifoQueue: true,
+        RedrivePolicy: {
+          maxReceiveCount: 3,
+          deadLetterTargetArn: Match.anyValue()
+        }
+      });
+    });
+
+    it('should create FIFO DLQ with correct configuration', () => {
+      template.hasResourceProperties('AWS::SQS::Queue', {
+        QueueName: 'test-fifo-queue-dlq.fifo',
+        MessageRetentionPeriod: 345600,
+        FifoQueue: true
+      });
+    });
+  });
+});
