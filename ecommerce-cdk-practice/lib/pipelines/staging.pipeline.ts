8693157f7d079a4dc4b0e9829968d316e0a31b6c
Huyen Cao <huyen.cao@asnet.com.vn>
2025-06-04 15:47:08 +0700

    feat(pipeline): update branch name for each pipeline

--- ecommerce-cdk-practice/lib/pipelines/staging.pipeline.ts
+++ ecommerce-cdk-practice/lib/pipelines/staging.pipeline.ts
@@ -17,11 +17,20 @@ export class StagingPipelineStack extends Stack {
     super(scope, id, props);
 
     const { stage } = props;
+
+    const { stageName } = stage;
+    // Get github repo and branch from context
+    const github = scope.node.tryGetContext('github');
+    const repo = github[stageName].repositoryName;
+    const branch = github[stageName].branch;
+
     // Create the pipeline
     const pipeline = PipelineHelper.createPipeline({
       scope: this,
-      pipelineName: `${APP_NAME}-pipeline-${stage.stageName}`,
-      stageName: stage.stageName
+      pipelineName: `${APP_NAME}-pipeline-${stageName}`,
+      stageName,
+      repo,
+      branch
     });
 
     // Add stage
