ce4c554e70a07ea15eaa71e550750ed1ff96b184
Huyen Cao <huyen.cao@asnet.com.vn>
2025-06-17 17:56:04 +0700

    feat(layer): create layer for lambda function

--- amplify-practice/amplify/backend.ts
+++ amplify-practice/amplify/backend.ts
@@ -3,16 +3,20 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { VpcConstruct } from './custom/vpc/resource';
 import { RdsConstruct } from './custom/rds/resource';
+import { LambdaLayerConstruct } from './custom/lambda/layer/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
-  auth,
+  auth
 });
 
 // Create a new stack for custom resources
 const customResourceStack = backend.createStack('CustomResourceStack');
+
+// Create a new Lambda layer
+const { layer } = new LambdaLayerConstruct(customResourceStack, 'LambdaLayerConstruct');
 
 // Create a new VPC and security group
 const { vpc, securityGroup } = new VpcConstruct(customResourceStack, 'VpcConstruct');
@@ -23,9 +27,11 @@ const rds = new RdsConstruct(customResourceStack, 'RdsConstruct', {
   securityGroup,
 });
 
+const rdsEndpoint = rds.instance.dbInstanceEndpointAddress;
+
 // Add output for backend
 backend.addOutput({
   custom: {
-    rdsEndpoint: rds.instance.dbInstanceEndpointAddress,
+    rdsEndpoint,
   },
 });
