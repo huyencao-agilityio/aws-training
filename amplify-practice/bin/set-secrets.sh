87ec9e80cde590f6238ab07a40c018c597609b24
Huyen Cao <huyen.cao@asnet.com.vn>
2025-06-19 17:45:35 +0700

    feat(api): define data in amplify to create appsync api

--- amplify-practice/bin/set-secrets.sh
+++ amplify-practice/bin/set-secrets.sh
@@ -2,7 +2,7 @@
 
 echo "Setting secrets"
 
-for key in GOOGLE_CLIENT_ID GOOGLE_CLIENT_SECRET FACEBOOK_CLIENT_ID FACEBOOK_CLIENT_SECRET; do
+for key in GOOGLE_CLIENT_ID GOOGLE_CLIENT_SECRET FACEBOOK_CLIENT_ID FACEBOOK_CLIENT_SECRET DATABASE_URL; do
   echo -n "${!key}" | tr -d '\n' | npx ampx sandbox secret set "$key"
 done
 
